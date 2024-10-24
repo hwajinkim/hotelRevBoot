e6ddd00612b0860646067d6c264dce526c971b8c
hwa.j <hwajin0720@naver.com>
2024-10-24 19:57:54 +0900

    JWT 토큰 만료 시 로그아웃

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -5,6 +5,15 @@ $(function(){
             $('.not-login').css('display', 'none');
 
             const payload = JSON.parse(atob(token.split('.')[1]));
+            console.log(payload);
+            const exp = payload.exp * 1000;
+
+            if(Date.now() >= exp){
+                alert("세션이 만료되었습니다. 다시 로그인 해주세요.");
+                localStorage.removeItem('token');
+                window.location.href="/common/loginForm";
+            }
+
             const userAuth = payload.auth;
 
             if(userAuth == 'ROLE_ADMIN'){
@@ -13,6 +22,9 @@ $(function(){
                 $('#admin-menu-1').css('display', 'none');
                 $('#admin-menu-2').css('display', 'none');
             }
+
+
+
         }else{
             $('.do-login').css('display', 'none');
             $('#user-menu').css('display', 'none');
