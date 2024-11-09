e38509f575e129ff44e6122bce9a0cb1129f7b9e
hwa.j <hwajin0720@naver.com>
2024-11-09 21:56:33 +0900

    redisConfig 수정

--- src/main/java/com/boot/sonorous/common/security/RedisConfig.java
+++ src/main/java/com/boot/sonorous/common/security/RedisConfig.java
@@ -4,12 +4,34 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
+        redisConfig.setPassword(redisPassword);
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setUseSsl(true);
+        return lettuceConnectionFactory;
+    }
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory){
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
